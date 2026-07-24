96b649fa43e18f4c1fb2a3d04a2088726e9cbc12
Arno <FateSpin@outlook.com>
2026-07-24 16:49:42 +0800

    feat: expand oidc application templates

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -1,14 +1,24 @@
+import { defaultClient } from './defaults.js';
+
 export const templates = {
   generic: {
-    public: false,
-    authorization_policy: 'two_factor',
-    require_pkce: true,
-    pkce_challenge_method: 'S256',
-    scopes: ['openid', 'email', 'profile'],
-    response_types: ['code'],
-    grant_types: ['authorization_code'],
-    access_token_signed_response_alg: 'none',
-    userinfo_signed_response_alg: 'none',
-    token_endpoint_auth_method: 'client_secret_post'
+    name: 'Generic OIDC',
+    config: { ...defaultClient }
+  },
+  memos: {
+    name: 'Memos',
+    config: { ...defaultClient }
+  },
+  hermes: {
+    name: 'Hermes Agent',
+    config: { ...defaultClient }
+  },
+  openwebui: {
+    name: 'Open WebUI',
+    config: { ...defaultClient }
+  },
+  grafana: {
+    name: 'Grafana',
+    config: { ...defaultClient }
   }
 };
