645d5e4982b815b9dd1922a1ce6c3d31c22e65fa
Arno <FateSpin@outlook.com>
2026-07-24 16:48:40 +0800

    feat: add oidc application templates

---
+++ src/lib/templates.js
@@ -0,0 +1,14 @@
+export const templates = {
+  generic: {
+    public: false,
+    authorization_policy: 'two_factor',
+    require_pkce: true,
+    pkce_challenge_method: 'S256',
+    scopes: ['openid', 'email', 'profile'],
+    response_types: ['code'],
+    grant_types: ['authorization_code'],
+    access_token_signed_response_alg: 'none',
+    userinfo_signed_response_alg: 'none',
+    token_endpoint_auth_method: 'client_secret_post'
+  }
+};
